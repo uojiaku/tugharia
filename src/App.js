33fda908a116bd2ceb2d530a08567ba6efa4e742
Uche <ucojiaku@gmail.com>
2022-07-04 03:36:13 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import odogwu from './odogwu.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={odogwu} className="App-logo" alt="logo" />
+        <p>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
